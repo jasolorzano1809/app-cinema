48f79995cc6b51fa25d3755983f51c5ff02f792c
José Alberto Solórzano Atitla <125301548+jasolorzano1809@users.noreply.github.com>
2023-09-27 19:43:31 -0600

    searchdelegate finish seccion 14

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -34,11 +34,19 @@ class CustomAppbar extends ConsumerWidget {
                 ),
                 const Spacer(),
                 IconButton(onPressed: () {
+                  
 
-                  final movieRepository = ref.read(movieRepositoryProvider);
-
-                  showSearch<Movie?>(context: context,
-                   delegate: SearchMovieDelegate(searchMovies: movieRepository.searchMovies)
+                  //todo
+                  final searchedMovies = ref.read(searchedMoviesProvider);
+                  final searchQuery = ref.read(searchQueryProvider);
+                  showSearch<Movie?>(
+                    query: searchQuery,
+                    context: context,
+                   delegate: SearchMovieDelegate(
+                    initialMovies: searchedMovies,
+                    searchMovies:ref.read(searchedMoviesProvider.notifier).searchMoviesByQuery
+                   
+                )
                    ).then((movie){
                       if(movie == null) return;
                       context.push('/movie/${ movie.id}');
